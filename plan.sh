89224f26c7e5b67c4068d41cf02d415d0a6cedfe
Liang <shangliang1996@gmail.com>
2024-07-21 14:28:55 -0700

    Update plan.sh

--- plan.sh
+++ plan.sh
@@ -1,5 +1,5 @@
 export nnUNet_raw_data_base='atlasv2/raw/'
-export nnUNet_preprocessed='/opt/algorithm/preprocessed'
+export nnUNet_preprocessed='atlasv2/preprocessed'
 export RESULTS_FOLDER='atlasv2/results/'
 
 nnUNet_plan_and_preprocess -t TASK_ID --verify_dataset_integrity
