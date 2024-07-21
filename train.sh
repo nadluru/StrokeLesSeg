8efe15c6fa1e560e4b3a1cde31a93b5b2c83a1ef
Liang <shangliang1996@gmail.com>
2024-07-21 13:51:06 -0700

    Update train.sh

--- train.sh
+++ train.sh
@@ -1,5 +1,5 @@
 export nnUNet_raw_data_base='atlasv2/raw/'
-export nnUNet_preprocessed='preprocessed'
+export nnUNet_preprocessed='/opt/algorithm/preprocessed'
 export RESULTS_FOLDER='atlasv2/results/'
 
-nnUNet_train 3d_fullres TRAINER TASK FOLDER --npz
+nnUNet_train 3d_fullres TRAINER TASK FOLD --npz
