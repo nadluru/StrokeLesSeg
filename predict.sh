5576e6836e70d8541147e3dcf351697289a535db
Liang <shangliang1996@gmail.com>
2024-07-21 14:29:06 -0700

    Update predict.sh

--- predict.sh
+++ predict.sh
@@ -2,7 +2,7 @@
 # SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 
 export nnUNet_raw_data_base='atlasv2/raw'
-export nnUNet_preprocessed='/opt/algorithm/preprocessed'
+export nnUNet_preprocessed='atlasv2/preprocessed'
 export RESULTS_FOLDER='atlasv2/results/'
 
 NNUNet_FOLDER="nnUNet/nnunet/"
